7173f800a72039f0dc79d354f102fe9e7e1d5a97
Ricardo Marinovic <marinovic.ricardo@gmail.com>
2021-02-15 00:47:03 -0300

    Add phx_gen_auth dependency

--- lib/playground_web/router.ex
+++ lib/playground_web/router.ex
@@ -1,6 +1,8 @@
 defmodule PlaygroundWeb.Router do
   use PlaygroundWeb, :router
 
+  import PlaygroundWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule PlaygroundWeb.Router do
     plug :put_root_layout, {PlaygroundWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -39,5 +42,37 @@ defmodule PlaygroundWeb.Router do
       pipe_through :browser
       live_dashboard "/dashboard", metrics: PlaygroundWeb.Telemetry
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", PlaygroundWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    get "/users/register", UserRegistrationController, :new
+    post "/users/register", UserRegistrationController, :create
+    get "/users/log_in", UserSessionController, :new
+    post "/users/log_in", UserSessionController, :create
+    get "/users/reset_password", UserResetPasswordController, :new
+    post "/users/reset_password", UserResetPasswordController, :create
+    get "/users/reset_password/:token", UserResetPasswordController, :edit
+    put "/users/reset_password/:token", UserResetPasswordController, :update
+  end
+
+  scope "/", PlaygroundWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    get "/users/settings", UserSettingsController, :edit
+    put "/users/settings", UserSettingsController, :update
+    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+  end
+
+  scope "/", PlaygroundWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+    get "/users/confirm", UserConfirmationController, :new
+    post "/users/confirm", UserConfirmationController, :create
+    get "/users/confirm/:token", UserConfirmationController, :confirm
   end
 end
