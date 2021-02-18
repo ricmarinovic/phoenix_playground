120c9eaa2caecd01697da2909de01fb97f143c52
Ricardo Marinovic <marinovic.ricardo@gmail.com>
2021-02-18 02:32:43 -0300

    Fix formatting on .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,4 @@
-import_if_available Ecto.Query, only: [from: 2]
-import_if_available Ecto.Changeset
+import_if_available(Ecto.Query, only: [from: 2])
+import_if_available(Ecto.Changeset)
 
 alias Playground.Repo
